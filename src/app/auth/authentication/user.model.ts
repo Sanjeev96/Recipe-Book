d467d892c5735501dc87f4d8df0125d23a27f426
sanjeeve1996 <sanjeev.dhillon@esure.com>
2019-09-28 22:24:26 +0100

    feat: user mode with logic to allow more than one instance of User

---
+++ src/app/auth/authentication/user.model.ts
@@ -0,0 +1,18 @@
+export class User { // created with class so we can use 'new' keyword
+    constructor(
+        public email: string,
+        public id: string,
+        private _token: string, // private so it cant be accessed from here
+        private _tokenEXPdate: Date,
+    ) {}
+
+    // getter to check VALIDITY of value and so user
+    // cant change value (unless a setter is used)
+    get token() {
+        if (!this._tokenEXPdate || this._tokenEXPdate < new Date())  {
+            return null;
+        } else {
+            return this._token; // if with conditions then return value 
+        }
+    }
+}
